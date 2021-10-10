06e4f9c2749888f98ef122cbb6385c53d89345b4
cuongbokreal <92067448+cuongbokreal@users.noreply.github.com>
2021-10-10 22:22:20 +0700

    Create dataAcc.js

---
+++ ctv/dataAcc.js
@@ -0,0 +1,14 @@
+var dataAcc=[
+  {"id":"00",
+   "mk":"00",
+   "name":"Cuongbokdeptrai",
+   "ref":"01",
+   "refBy":"",
+  },
+  {"id":"01",
+   "mk":"02",
+   "name":"Chaoem",
+   "ref":"02",
+   "refBy":"00",
+  },
+]

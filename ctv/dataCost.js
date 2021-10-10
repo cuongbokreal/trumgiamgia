7f6f46791d727681a7ce17295e58c0b9cd721a93
cuongbokreal <92067448+cuongbokreal@users.noreply.github.com>
2021-10-10 19:28:32 +0700

    Create dataCost.js

---
+++ ctv/dataCost.js
@@ -0,0 +1,117 @@
+var dataChinhSachHoaHong = [
+  {"shopee":[
+    {"cate":"Ô tô - Xe máy - Xe đạp","nc":"7.36", "ec":"2.16"},
+    {"cate":"Túi ví","nc":"9.28", "ec":"2.72"},
+    {"cate":"Nhà sách Online","nc":"7.36", "ec":"1.0"},
+    {"cate":"Máy ảnh - Máy quay phim	","nc":"3.68", "ec":"1.12"},
+    {"cate":"Máy tính & Laptop","nc":"3.68", "ec":"1.12"},
+    {"cate":"Thiết bị điện tử","nc":"6.64", "ec":"0.96"},
+    {"cate":"Phụ kiện thời trang","nc":"9.28", "ec":"2.0"},
+    {"cate":"Bách hóa online","nc":"6.0", "ec":"0.96"},
+    {"cate":"Sức khỏe & Sắc đẹp","nc":"10.0", "ec":"0.96"},
+    {"cate":"Nhà cửa & Đời sống","nc":"7.36", "ec":"1.12"},
+    {"cate":"Thiết bị gia dụng","nc":"6.64", "ec":"1.12"},
+    {"cate":"Thời Trang Nam","nc":"9.28", "ec":"1.12"},
+    {"cate":"Giày dép Nam","nc":"8.88", "ec":"2.0"},
+    {"cate":"Điện thoại & Phụ kiện","nc":"3.2", "ec":"0.8"},
+    {"cate":"Mẹ & Bé","nc":"6.48", "ec":"0.96"},
+    {"cate":"Thể thao & Du lịch","nc":"9.28", "ec":"2.72"},
+    {"cate":"Đồ chơi","nc":"7.36", "ec":"1.2"},
+    {"cate":"Đồng hồ","nc":"8.88", "ec":"3.6"},
+    {"cate":"Thời trang Nữ","nc":"10.00", "ec":"1.2"},
+    {"cate":"Giày dép Nữ","nc":"8.88", "ec":"3.6"},
+    {"cate":"Khác","nc":"0", "ec":"0"},
+   ]
+  }, //0:shopee
+  {"tiki":[
+    {"cate":"Nhà cửa đời sống","web":"2.00", "app":"8.00"},
+    {"cate":"Bách hóa online", "web":"1.2", "app":"4.4"},
+    {"cate":"Balo và Vali","web":"", "app":""},
+    {"cate":"Giày - Dép nam", "web":"", "app":""},
+    {"cate":"Giày - Dép nam","web":"", "app":""},
+    {"cate":"", "web":"", "app":""},
+    {"cate":"","web":"", "app":""},
+    {"cate":"", "web":"", "app":""},
+    {"cate":"","web":"", "app":""},
+    {"cate":"", "web":"", "app":""},
+    {"cate":"","web":"", "app":""},
+    {"cate":"", "web":"", "app":""},
+    {"cate":"","web":"", "app":""},
+    {"cate":"", "web":"", "app":""},
+    {"cate":"","web":"", "app":""},
+    {"cate":"", "web":"", "app":""},
+    {"cate":"","web":"", "app":""},
+    {"cate":"", "web":"", "app":""},
+    {"cate":"","web":"", "app":""},
+    {"cate":"", "web":"", "app":""},
+    {"cate":"","web":"", "app":""},
+    {"cate":"", "web":"", "app":""},
+    {"cate":"","web":"", "app":""},
+    {"cate":"", "web":"", "app":""},
+    {"cate":"","web":"", "app":""},
+    {"cate":"", "web":"", "app":""},
+    {"cate":"","web":"", "app":""},
+    {"cate":"", "web":"", "app":""},
+    {"cate":"","web":"", "app":""},
+    {"cate":"", "web":"", "app":""},
+    {"cate":"","web":"", "app":""},
+    {"cate":"","web":"", "app":""},
+   ]    
+  }, //1: tiki
+  {"lazada":[
+    {"cate":"Bags and Travel","nc":"14.40", "ec":"6.40"},
+    {"cate":"Men's Shoes and Clothing","nc":"14.4", "ec":"6.4"},
+    {"cate":"Sports & Outdoors","nc":"14.4", "ec":"6.4"},
+    {"cate":"Watches Sunglasses Jewellery","nc":"14.4", "ec":"6.4"},
+    {"cate":"Women's Shoes and Clothing","nc":"14.4", "ec":"6.4"},
+    {"cate":"Sports Shoes and Clothing","nc":"14.4", "ec":"6.4"},
+    {"cate":"Bedding & Bath","nc":"12.8", "ec":"5.6"},
+    {"cate":"Furniture & Organization","nc":"12.8", "ec":"5.6"},
+    {"cate":"Kitchen & Dining	","nc":"12.8", "ec":"5.6"},
+    {"cate":"Laundry & Cleaning Equipment","nc":"12.8", "ec":"5.6"},
+    {"cate":"Lighting & Décor","nc":"12.8", "ec":"5.6"},
+    {"cate":"Media, Music & Books	","nc":"12.8", "ec":"5.6"},
+    {"cate":"Motors","nc":"12.8", "ec":"5.6"},
+    {"cate":"Outdoor & Garden	","nc":"12.8", "ec":"5.6"},
+    {"cate":"Stationery & Craft	","nc":"12.8", "ec":"5.6"},
+    {"cate":"Tools & Home Improvement	","nc":"12.8", "ec":"5.6"},
+    {"cate":"Groceries","nc":"9.6", "ec":"4.0"},
+    {"cate":"Household Supplies","nc":"9.6", "ec":"4.0"},
+    {"cate":"Pet Supplies","nc":"9.6", "ec":"4.0"},
+    {"cate":"Mother & Baby","nc":"6.4", "ec":"2.4"},
+    {"cate":"Toys & Games","nc":"6.4", "ec":"2.4"},
+    {"cate":"Audio","nc":"6.4", "ec":"2.4"},
+    {"cate":"Cameras & Drones","nc":"6.4", "ec":"2.4"},
+    {"cate":"Computers & Laptops","nc":"6.4", "ec":"2.4"},
+    {"cate":"Data Storage","nc":"6.4", "ec":"2.4"},
+    {"cate":"Large Appliances","nc":"6.4", "ec":"2.4"},
+    {"cate":"Mobiles & Tablets","nc":"6.4", "ec":"2.4"},
+    {"cate":"Monitors & Printers","nc":"6.4", "ec":"2.4"},
+    {"cate":"Small Appliances","nc":"6.4", "ec":"2.4"},
+    {"cate":"Smart Devices","nc":"6.4", "ec":"2.4"},
+    {"cate":"Televisions & Videos","nc":"6.4", "ec":"2.4"},
+    {"cate":"Electronics Accessories","nc":"6.4", "ec":"2.4"},
+    {"cate":"Khác","nc":"0", "ec":"0"},
+   ]    
+  },//2:lazada
+];
+const tiLe = 55/100;
+var innerChinhSachHoaHongShopee = `<tr><td>Category</td><td>Khách hàng mới (%)</td><td>Khách hàng cũ (%)</td></tr>`;
+  for(let i=0;i<dataChinhSachHoaHong[0].shopee.length;i++){
+    innerChinhSachHoaHongShopee +=`<tr><td>${dataChinhSachHoaHong[0].shopee[i].cate}</td><td>${(dataChinhSachHoaHong[0].shopee[i].nc * tiLe).toFixed(1)}</td><td>${(dataChinhSachHoaHong[0].shopee[i].ec * tiLe).toFixed(1)}</td></tr>`;
+  }
+var innerChinhSachHoaHongTiki = `<tr><td>Category</td><td>Web (%)</td><td>App (%)</td></tr>`;
+  for(let i=0;i<dataChinhSachHoaHong[1].tiki.length;i++){
+    innerChinhSachHoaHongTiki +=`<tr><td>${dataChinhSachHoaHong[1].tiki[i].cate}</td><td>${(dataChinhSachHoaHong[1].tiki[i].web * tiLe).toFixed(1)}</td><td>${(dataChinhSachHoaHong[1].tiki[i].app * tiLe).toFixed(1)}</td></tr>`;
+  }
+var innerChinhSachHoaHongLazada = `<tr><td>Category</td><td>Khách hàng mới (%)</td><td>Khách hàng cũ (%)</td></tr>`;
+for(let i=0;i<dataChinhSachHoaHong[2].lazada.length;i++){
+    innerChinhSachHoaHongLazada +=`<tr><td>${dataChinhSachHoaHong[2].lazada[i].cate}</td><td>${(dataChinhSachHoaHong[2].lazada[i].nc * tiLe).toFixed(1)}</td><td>${(dataChinhSachHoaHong[2].lazada[i].ec * tiLe).toFixed(1)}</td></tr>`;
+  }
+
+  document.getElementById('innerChinhSachHoaHong').innerHTML = `Shopee: <br/> <table>${innerChinhSachHoaHongShopee}</table><br/>
+                                                                Tiki: <br/> <table>${innerChinhSachHoaHongTiki}</table><br/>
+                                                                Lazada: <br/> <table>${innerChinhSachHoaHongLazada}</table>
+                                                                Hoa hồng tối đa trên 1 đơn hàng: 80.000 VND
+                                                                `;
+

bcacc690f2ce5f00761f3e5c85510297f3d4cfda
cuongbokreal <92067448+cuongbokreal@users.noreply.github.com>
2021-10-10 23:25:42 +0700

    Update dataAcc.js

--- ctv/login/dataAcc.js
+++ ctv/login/dataAcc.js
@@ -6,28 +6,6 @@ var dataLogAcc=[
    "mk":"02" 
   },
 ];
-const loginTab = document.getElementById('login');
-const sigupTab = document.getElementById('sigup');
-function login(){
-  document.getElementById('thbao').innerHTML = '';
-  loginTab.classList.add('active');
-  sigupTab.classList.remove('active');
-  document.title="Đăng nhập - Affiliate cùng Cuongbok";
-  document.getElementById('mainLog').innerHTML = `<h1>Đăng nhập để sử dụng dịch vụ</h1>
-                                                  <input id="taikhoan" class="fadeIn second" name="login" type="number" placeholder="id của bạn">
-                                                  <input id="matkhau" class="fadeIn third" name="login" type="password" placeholder="mật khẩu"><br/><br/>
-                                                  <button onclick="dangnhap(tk,mk)" class="btn-login"class="fadeIn fourth" value="Đăng nhập">Đăng nhập</button>
-                                                  <div id="formFooter">
-                                                  <a class="underlineHover" href="//m.me/cuongbok">Forgot Password?</a>
-                                                  </div>`;}
-function sigup(){
-  document.getElementById('thbao').innerHTML = '';
-  sigupTab.classList.add('active');
-  loginTab.classList.remove('active');
-  document.title="Đăng ký - Affiliate cùng Cuongbok";
-  document.getElementById('mainLog').innerHTML = `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfand3iU0bug838GO4LcWLWophsqs3UPrfugVMiExKeIUoBvg/viewform?embedded=true" 
-  width="800px" height="1150px" frameborder="0" marginheight="0" marginwidth="0">Đăng ký</iframe><br/><br/>`;
-}
 
 var getUrlParameter = function getUrlParameter(sParam) {
     var sPageURL = window.location.search.substring(1),

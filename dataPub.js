e3537794e802ff2084d996fb5c58f9b5260b4329
cuongbokreal <92067448+cuongbokreal@users.noreply.github.com>
2021-10-15 18:45:36 +0700

    Update dataPub.js

--- dataPub.js
+++ dataPub.js
@@ -32,5 +32,5 @@ document.body.appendChild(jsMain);
 $(window).on('load', function(event) {
    $('body').removeClass('preloading');
       // $('.load').delay(1000).fadeOut('fast');
-   $('.loader').delay(1000).fadeOut('fast');
+   $('.loader').delay(500).fadeOut('fast');
 });

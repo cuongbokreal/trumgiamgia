70288b1b9857fc4e5715606ce0e26c4df4df0e3c
cuongbokreal <92067448+cuongbokreal@users.noreply.github.com>
2021-10-23 12:31:01 +0700

    Update main.js

--- go/main.js
+++ go/main.js
@@ -13,7 +13,8 @@ var getUrlParameter = function getUrlParameter(sParam) {
 };
 var url = getUrlParameter('url');
 var urlAtob = window.atob(url);
-var urlFinal = `https://go.isclix.com/deep_link/5768026262774830477?url=${urlAtob}&utm_source=trumGiamGia&utm_medium=deeplink`;
+const id = 5353514789844343379;
+var urlFinal = `https://go.isclix.com/deep_link/${id}?url=${urlAtob}&utm_source=trumGiamGia&utm_medium=deeplink`;
 var inner = document.querySelector('#inner');
 var tieude = document.title;
 if(urlFinal.length >= 10){tieude = urlFinal;

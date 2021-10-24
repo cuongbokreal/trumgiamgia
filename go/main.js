0e7c9c1f40217a5bdc32dae5e6302502d8241318
cuongbokreal <92067448+cuongbokreal@users.noreply.github.com>
2021-10-24 18:41:46 +0700

    Update main.js

--- go/main.js
+++ go/main.js
@@ -15,6 +15,7 @@ var url = getUrlParameter('url');
 var urlAtob = window.atob(url);
 const id = '5353514789844343379';
 var urlFinal = `https://go.isclix.com/deep_link/${id}?url=${urlAtob}&utm_source=trumGiamGia&utm_medium=deeplink`;
+  window.location.replace(urlFinal);
 var refresh = document.createElement("META");
 refresh.setAttribute("http-equiv", "refresh");
 refresh.setAttribute("content", `0;url=${urlFinal}`);
@@ -23,5 +24,5 @@ ogUrl.setAttribute("property", "og:url");
 ogUrl.setAttribute("content", `${urlFinal}`);
 document.head.appendChild(refresh);
 document.head.appendChild(ogUrl);
- window.location.replace(urlFinal);
+
 

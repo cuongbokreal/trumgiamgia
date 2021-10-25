48ac54319a9a1765f66c06bdf657ca9d26f61932
cuongbokreal <92067448+cuongbokreal@users.noreply.github.com>
2021-10-25 15:35:02 +0700

    Update dataPub.js

--- dataPub.js
+++ dataPub.js
@@ -29,8 +29,4 @@ if(ctv){
 }
 document.body.appendChild(jsMain); 
 
-$(window).on('load', function(event) {
-   $('body').removeClass('preloading');
-      // $('.load').delay(1000).fadeOut('fast');
-   $('.loader').delay(350).fadeOut('fast');
-});
+

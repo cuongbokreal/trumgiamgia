f240277329053bd5c44f3f4da3fe462ee9742b9b
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-11-02 20:22:43 +0700

    Update dataPub.js

--- dataPub.js
+++ dataPub.js
@@ -37,7 +37,6 @@ var listBgColor = ["ffeeee",//hồng nhạt
     "#eee3ff",//tím hơi nhạt
     "#b7b6e4",//tím hơi đậm
     "#ffe3eb",//hồng hơi nhạt
-    "#d4bdb9",//nâu nhạt
     "#c5ce67",//xanh cức
     "#f38b8b",//đỏ tươi
     "#8bcff3",//xanh facebook

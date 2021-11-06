2df405cbf9e42c92d42133cfe1cdd8ed388b0192
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-11-06 19:39:13 +0700

    Update blogger-post.js

--- api/get-code/blogger-post.js
+++ api/get-code/blogger-post.js
@@ -32,3 +32,13 @@ function blockContextMenu(){
 	document.addEventListener('contextmenu', event => event.preventDefault());  
 }
 blockContextMenu()
+
+/*function check(e){
+    alert(e.keyCode);
+}*/
+document.onkeydown = function(e) {
+        if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {//Alt+c, Alt+v will also be disabled sadly.
+            alert('not allowed');
+        }
+        return false;
+};

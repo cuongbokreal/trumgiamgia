ff0b5f1d88da5e33f67dde64232865962e074713
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-11-06 19:46:54 +0700

    Update blogger-post.js

--- api/get-code/blogger-post.js
+++ api/get-code/blogger-post.js
@@ -31,10 +31,12 @@
 function blockContextMenu(){
 	document.addEventListener('contextmenu', event => event.preventDefault());
 	document.onkeydown = function(e) {
-        if (e.ctrlKey && (e.keyCode === 85 || e.keyCode === 117)) {//Alt+c, Alt+v will also be disabled sadly.
+        if ((e.keyCode === 85 || e.keyCode === 117)) {//Alt+c, Alt+v will also be disabled sadly.
             console.log('Chặn!');
         }
         return false;
 };
 }
 blockContextMenu()
+
+//'e.ctrlKey &&' chặn Ctrl 

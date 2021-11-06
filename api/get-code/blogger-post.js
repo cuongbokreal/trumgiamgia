e4ef903324b235d984c988ed51b5a06a7005d1d2
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-11-06 18:13:54 +0700

    Update blogger-post.js

--- api/get-code/blogger-post.js
+++ api/get-code/blogger-post.js
@@ -23,3 +23,20 @@
 	document.body.setAttribute("oncopy", "copyVoucherCode()");
     	mainCode.setAttribute("oncopy", "copyVoucherCode()");    
     }, 3000);
+
+//block rightmouse
+var message="NoRightClicking"; 
+function defeatIE() {
+	if (document.all) {(message);return false;}
+} 
+function defeatNS(e) {
+	if (document.layers||(document.getElementById&&!document.all)) { 
+		if (e.which==2||e.which==3) {(message);return false;}
+	}
+} 
+if (document.layers) {
+	document.captureEvents(Event.MOUSEDOWN);
+	document.onmousedown=defeatNS;} 
+else{document.onmouseup=defeatNS;document.oncontextmenu=defeatIE;
+    } 
+document.oncontextmenu=new Function("return false")

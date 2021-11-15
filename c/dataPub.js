c605a57a6ff054aca9bc5dbcd68c2dffa0c83c92
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-11-15 17:37:26 +0700

    Create dataPub.js

---
+++ c/dataPub.js
@@ -0,0 +1,13 @@
+var dataPub = [
+  {"id":"02",
+   "code":"9q5w",
+   "name":"Nguyễn Thị Quỳnh Anh",
+   "lh":"100027386360545",
+  },
+  {"id":"03",
+   "code":"",
+   "name":"",
+   "lh":"",
+  },
+
+]

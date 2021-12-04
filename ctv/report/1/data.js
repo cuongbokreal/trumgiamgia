a1db1043e7d0bfc798c7f5e942d31c038ee48260
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-12-04 10:31:02 +0700

    Update data.js

--- ctv/report/1/data.js
+++ ctv/report/1/data.js
@@ -8,14 +8,14 @@ var dataDoanhThu =[
 ];
 
 var dataDonHang = [
-  {"thoiGianMua":"",
-   "maDonHang":"",
-   "maDanhMuc":"",
+  {"thoiGianMua":"2021/12/02 20:39:11",
+   "maDonHang":"2112024D4HTTJQ",
+   "maDanhMuc":"Suc_Khoe_&_Sac_Dep",
    "giaTriDonHang":0,
    "hoaHong":0,
-   "adver":"",
-   "ngayDuyet":"",
+   "adver":"shopee",
+   "ngayDuyet":"18/2/2022",
    "liDoHuy":"",
-   "loaiThietBi":"",
+   "loaiThietBi":"mobile",
   },
 ];

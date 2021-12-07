9946ac7eaf8fcedaed8f274083627dc65b23abfc
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-12-07 15:40:41 +0700

    Create auto-search-link-sp.js

---
+++ works/auto-search-link-sp.js
@@ -0,0 +1,6 @@
+var linkSearch = `https://shopee.vn/%C3%81o-gi%E1%BB%AF-nhi%E1%BB%87t-k%E1%BA%BB-n%E1%BB%AF-b%E1%BA%A3ng-16-m%C3%A0u-%C4%91%E1%BA%B9p-nh%E1%BA%A5t-ng%C3%A0nh-%C3%A1o-k%E1%BA%BB-n%E1%BB%AF-c%E1%BB%95-3p-phong-c%C3%A1ch-H%C3%A0n-Qu%E1%BB%91c-Kanimi-i.452860550.9995411486?sp_atk=f3074004-c5cf-4a6b-a078-80d036aaf384&xptdk=f3074004-c5cf-4a6b-a078-80d036aaf384`;
+var inputSearch = document.getElementById('atEQPOIVFSDFSDG-search');
+var buttonSearch = document.getElementsByClassName('atEQPOIVFSDFSDG-btn-search')[0];
+
+inputSearch.value = linkSearch;
+buttonSearch.click();

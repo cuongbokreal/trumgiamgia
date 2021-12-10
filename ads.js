709a59940cb47d00148254af8838285216bc30e5
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-12-10 15:23:05 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -60,6 +60,7 @@ var innerBanner2 = document.getElementById('innerBanner2');
 var innerBanner3 = document.getElementById('innerBanner3');
 
 if(innerBanner1){
+    if(randomBannerTop == 0){randomBannerTop = 1}
     randomBannerTop = randomBannerTop +1;
     if( randomBannerTop > dataBannerTop.length){randomBannerTop = 0}
     console.log(randomBannerTop);

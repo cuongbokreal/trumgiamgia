56e6ead8e8f1e044e6dfbd22536bd49fabf7ffb2
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-12-13 17:02:47 +0700

    Create data.js

---
+++ ctv/report/2/data.js
@@ -0,0 +1,21 @@
+var dataDoanhThu =[
+  {"thangDoiSoat":"",
+   "hoaHongDuocDuyet":0,
+   "phatSinhDuocDuyet":0,
+   "tamUngThanhToan":0,
+   "tonDongDuocDuyet":0,
+  },
+];
+
+var dataDonHang = [
+  {"thoiGianMua":"2021/12/12 00:09:01	",
+   "maDonHang":"211212UD219DGP",
+   "maDanhMuc":"Phu_Kien_Thoi_Trang	",
+   "giaTriDonHang":1500,
+   "hoaHong":30,
+   "adver":"shopee",
+   "ngayDuyet":"18/2/2022",
+   "liDoHuy":"",
+   "loaiThietBi":"mobile_app",
+  },
+];

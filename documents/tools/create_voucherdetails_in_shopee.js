93b27cf18e7dcf3fc80b4b30134f813ebf1aa3c8
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-12-16 09:31:09 +0700

    Create create_voucherdetails_in_shopee.js

---
+++ documents/tools/create_voucherdetails_in_shopee.js
@@ -0,0 +1,34 @@
+var inputKetQua = document.createElement('input');
+inputKetQua.setAttribute("id", "inputKetQua");
+inputKetQua.setAttribute("onclick", "copyKetQua()");
+inputKetQua.setAttribute("style", "text-align: center;width: 100%;");
+document.body.append(inputKetQua)
+
+var getHeader = document.querySelectorAll('#main > div > div > div > div > div > div > div');
+var getTitle = getHeader[1].innerText;
+var getBrand = getHeader[0].innerText;
+var getBody =  document.querySelectorAll('#main > div > div > div > div > div > div');
+var getDes = getBody[8].innerText;
+var getImg = document.querySelectorAll('#main > div > div > div > div > div > div > div > div > div')[0].getAttribute('style');
+getImg = getImg.replaceAll('background-image: url("','');
+getImg = getImg.replaceAll('"); background-size: contain; background-repeat: no-repeat;','');
+var getHref = document.querySelector('div > button > a').href;
+
+var ketQua = `https://trumgiamgia.tk/voucher-details?vc_title=${getTitle}&vc_des=${getDes}&vc_brand=${getBrand}&vc_code=&vc_brand_image=${getImg}&vc_href=${window.btoa(getHref)}&ctvId=Cuongbok`;
+
+getTitle = getTitle.replaceAll("%","%25");
+getTitle = getTitle.replaceAll("&","%26");
+getDes = getDes.replaceAll("%","%25");
+getDes = getDes.replaceAll("&","%26");
+ketQua = ketQua.replaceAll(" ","%20");
+
+inputKetQua.value = ketQua;
+
+function copyKetQua(){
+	var copyText = document.getElementById("inputKetQua");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        navigator.clipboard.writeText(copyText.value);
+        console.log("Copied the: " + copyText.value);
+}
+

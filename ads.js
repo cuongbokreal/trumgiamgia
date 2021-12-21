dcae8c2be511cb2ce314860261ecec11aec9ccc5
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-12-21 18:16:00 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -55,10 +55,28 @@ var innerBannerTop = `
 
 document.getElementById('innerBannerTop').innerHTML = innerBannerTop;
 
+var adsBanner = document.getElementsByClassName('adsBanner');
+/*
 var innerBanner1 = document.getElementById('innerBanner1');
 var innerBanner2 = document.getElementById('innerBanner2');
 var innerBanner3 = document.getElementById('innerBanner3');
+*/
+for(let i=1; i<adsBanner.length; i++){
+    var adsId = document.getElementById(`innerBanner${i}`);
+    if(adsId){
+        randomBannerTop = randomBannerTop +1;
+        if( randomBannerTop >= dataBannerTop.length){randomBannerTop = 0}
+        console.log(randomBannerTop);
+        adsId.innerHTML = `
+    <a href="${dataBannerTop[randomBannerTop].href}" alt="${dataBannerTop[randomBannerTop].title}" title="${dataBannerTop[randomBannerTop].title}" target="_blank">
+    <img alt="${dataBannerTop[randomBannerTop].title}" title="${dataBannerTop[randomBannerTop].title}" src="${dataBannerTop[randomBannerTop].imgUrl}" width="100%" height="auto" />
+    </a>
+    <div class="closeBanner" onclick="closeBanner('innerBanner1')">
+    <svg class="closeX" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
+    </div>`;}
+}
 
+/*
 if(innerBanner1){
     randomBannerTop = randomBannerTop +1;
     if( randomBannerTop >= dataBannerTop.length){randomBannerTop = 0}
@@ -95,4 +113,4 @@ if(innerBanner3){
 <svg class="closeX" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
 </div>`;}
 
-
+*/

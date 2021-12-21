061c2411a5ed8e352de6ec8145f9eccc1ecd3478
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-12-21 18:30:28 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -43,8 +43,6 @@ var dataBannerTop = [
     "imgUrl":"https://i.imgur.com/PESbnzf.png",
     "href":"https://shorten.asia/BF4cyCFq",
     },
-
-
 ]
 var randomBannerTop = Math.floor(Math.random() * dataBannerTop.length);
 console.log(randomBannerTop);
@@ -55,7 +53,6 @@ var innerBannerTop = `
 document.getElementById('innerBannerTop').innerHTML = innerBannerTop;
 
 var adsBanner = document.getElementsByClassName('adsBanner');
-
 for(let i=1; i<adsBanner.length; i++){
     var adsId = document.getElementById(`innerBanner${i}`);
     if(adsId){
@@ -70,3 +67,4 @@ for(let i=1; i<adsBanner.length; i++){
     <svg class="closeX" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
     </div>`;}
 }
+

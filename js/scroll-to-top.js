8b547fc65a9c893826a1f13adddd964227848130
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2021-12-21 16:22:26 +0700

    Create scroll-to-top.js

---
+++ js/scroll-to-top.js
@@ -0,0 +1,13 @@
+var scrollToTopButton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}

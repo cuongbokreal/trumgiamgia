721e80586968372603920d4ffd298e30e223857c
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-04-19 13:10:15 +0700

    Update main.js

--- tools/get-voucher-tiki/main.js
+++ tools/get-voucher-tiki/main.js
@@ -4,9 +4,9 @@ var labelVoucher = document.querySelectorAll('[data-brick-type="COUPON"] > div >
 
 var imageVoucher = document.querySelectorAll('[data-brick-type="COUPON"] > div > div > div > div > div > div > div > div > div > div :nth-child(1) > img'); //.getAttribute('src')
 
-var imageVoucher = document.querySelectorAll('[data-brick-type="COUPON"] > div > div > div > div > div > div > div > div > div > div :nth-child(1) > img');
+var titleVoucher = document.querySelectorAll('[data-brick-type="COUPON"] > div > div > div > div > div > div > div > div > div :nth-child(3)'); //titleVoucher[i] = titleVoucher[i] .replace(``," ");
 
-console.log()
+console.log(titleVoucher[0].innerText)
 
 var detailVoucher = document.querySelectorAll('div > button > img')
 var popupRoot = document.getElementById('popup-root');

13c3818f1085dcd2ef97f13768a5aaa144b3ee45
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-04-19 12:46:04 +0700

    Update main.js

--- tools/get-voucher-tiki/main.js
+++ tools/get-voucher-tiki/main.js
@@ -1,11 +1,8 @@
-//jQuey để mouseOver
-var jQuery = '<script src="https://code.jquery.com/jquery-3.5.0.js"></script>';
-document.body.innerHTML += jQuery;
-
 var detailVoucher = document.querySelectorAll('div > button > img')
 var popupRoot = document.getElementById('popup-root');
 
 for(let i=0; i<detailVoucher.length; i++){
-  	detailVoucher[i].mouseover();
-    console.log(popupRoot.innerHTML)
+  	detailVoucher[i].addEventListener("mouseover", function() {
+    	console.log(popupRoot.innerHTML)
+        })
 }

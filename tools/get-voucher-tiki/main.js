58490a96c350627a2954fbaf60e0f2abf9f17592
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-04-19 13:27:31 +0700

    Update main.js

--- tools/get-voucher-tiki/main.js
+++ tools/get-voucher-tiki/main.js
@@ -1,20 +1,20 @@
-var voucher = document.querySelectorAll('[data-brick-type="COUPON"] > div > div > div > div > div');
+var voucher = '[data-brick-type="COUPON"] > div > div > div > div > div > div > div';
 
-var labelVoucher = document.querySelectorAll('[data-brick-type="COUPON"] > div > div > div > div > div > div > div > div > div > div > div');
+var imageVoucher = document.querySelectorAll(`${voucher} > div > div > div > div > img`);
 
-var imageVoucher = document.querySelectorAll('[data-brick-type="COUPON"] > div > div > div > div > div > div > div > div > div > div :nth-child(1) > img'); //.getAttribute('src')
+var titleVoucher = document.querySelectorAll(`${voucher} > div > div :nth-child(3)`);
 
-var titleVoucher = document.querySelectorAll('[data-brick-type="COUPON"] > div > div > div > div > div > div > div > div > div :nth-child(3)'); //titleVoucher[i] = titleVoucher[i] .replace(``," ");
 
-console.log(titleVoucher[0].innerText)
+console.log(titleVoucher[0], imageVoucher[0]);
+
 
 var detailVoucher = document.querySelectorAll('div > button > img')
 var popupRoot = document.getElementById('popup-root');
 
 for(let i=0; i<detailVoucher.length; i++){
   	detailVoucher[i].addEventListener("mouseover", function() {
-    var deatilsVoucher = popupRoot.innerHTML;
+    var deatilsVoucher = popupRoot.innerText;
     
-    	console.log()
+    console.log(deatilsVoucher)
         })
 }

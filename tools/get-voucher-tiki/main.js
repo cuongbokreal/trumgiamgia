c4bc3f56771c2909a321048fa9f43a1e2481c896
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-04-19 13:03:13 +0700

    Update main.js

--- tools/get-voucher-tiki/main.js
+++ tools/get-voucher-tiki/main.js
@@ -1,8 +1,20 @@
+var voucher = document.querySelectorAll('[data-brick-type="COUPON"] > div > div > div > div > div');
+
+var labelVoucher = document.querySelectorAll('[data-brick-type="COUPON"] > div > div > div > div > div > div > div > div > div > div > div');
+
+var imageVoucher = document.querySelectorAll('[data-brick-type="COUPON"] > div > div > div > div > div > div > div > div > div > div :nth-child(1) > img'); //.getAttribute('src')
+
+var imageVoucher = document.querySelectorAll('[data-brick-type="COUPON"] > div > div > div > div > div > div > div > div > div > div :nth-child(1) > img');
+
+console.log()
+
 var detailVoucher = document.querySelectorAll('div > button > img')
 var popupRoot = document.getElementById('popup-root');
 
 for(let i=0; i<detailVoucher.length; i++){
   	detailVoucher[i].addEventListener("mouseover", function() {
-    	console.log(popupRoot.innerHTML)
+    var deatilsVoucher = popupRoot.innerHTML;
+    
+    	console.log()
         })
 }

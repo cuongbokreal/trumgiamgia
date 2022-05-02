e0aefd47be7b266c8a2b2746cc6b85122c8b04a7
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-05-02 10:04:57 +0700

    Create data.js

---
+++ api/tong-hop/footer/data.js
@@ -0,0 +1,24 @@
+var updateTime = new Date();
+var idTime = `${updateTime.getDate()}${updateTime.getMonth()}`;
+var dataFooter = `
+
+📢 Lưu ý nho nhỏ AD có thêm mấy nhóm gửi mã + deal nhắc mã khung giờ liên tục mọi người tham gia tránh bỏ lở nhưng thứ hay ho ở nhóm này nhé
+
+👉 Nhóm tin nhanh mã (chỉ nhận tin từ ad, no chat)
+TGG 3 ➡ https://zalo.me/g/fqehfa279
+TGG 2 ➡ https://zalo.me/g/cvpxza602
+👉 Nhóm chát ➡ https://zalo.me/g/vjliqt213
+
+--------------------------------------------------------
+
+😍 Tham gia Group cập nhật Voucher, Deal xịn, duyệt bài tự động 💖
+👉 https://www.facebook.com/584602429633228
+👉 Group Za.lo cập nhật mã sớm nhất: https://zalo.me/g/fqehfa279
+
+🍀 Tìm mã giảm giá tại: https://topvoucher.tk/
+👉 Mã giảm giá SHOPEE: https://topvoucher.tk/ma-giam-gia/shopee/
+👉 Mã giảm giá LAZADA: https://topvoucher.tk/ma-giam-gia/lazada/
+👉 Mã giảm giá TIKI: https://topvoucher.tk/ma-giam-gia/tiki/
+${idTime}
+CTVID: TOPVOUCHERM001-CB
+`;

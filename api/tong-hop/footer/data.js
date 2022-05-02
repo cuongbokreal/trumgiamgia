38e07a843b4247f072e8f53240c4ba29cfed9d99
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-05-02 10:11:26 +0700

    Update data.js

--- api/tong-hop/footer/data.js
+++ api/tong-hop/footer/data.js
@@ -2,26 +2,19 @@ var updateTime = new Date();
 var idTime = `${updateTime.getDate()}${updateTime.getMonth()}`;
 var dataFooter = `
 
-📢 Lưu ý nho nhỏ AD có thêm mấy nhóm gửi mã + deal nhắc mã khung giờ liên tục mọi người tham gia tránh bỏ lở nhưng thứ hay ho ở nhóm này nhé
-
-👉 Nhóm tin nhanh mã (chỉ nhận tin từ ad, no chat)
-TGG 3 ➡ https://zalo.me/g/fqehfa279
-TGG 2 ➡ https://zalo.me/g/cvpxza602
-👉 Nhóm chát ➡ https://zalo.me/g/vjliqt213
-
---------------------------------------------------------
-
-😍 Tham gia Group cập nhật Voucher, Deal xịn, duyệt bài tự động 💖
-👉 https://www.facebook.com/584602429633228
-👉 Group Za.lo cập nhật mã sớm nhất: https://zalo.me/g/fqehfa279
-
-🍀 Tìm mã giảm giá tại: https://topvoucher.tk/
-👉 Mã giảm giá SHOPEE: https://topvoucher.tk/ma-giam-gia/shopee/
-👉 Mã giảm giá LAZADA: https://topvoucher.tk/ma-giam-gia/lazada/
-👉 Mã giảm giá TIKI: https://topvoucher.tk/ma-giam-gia/tiki/
+<p>📢 Lưu ý nho nhỏ AD có thêm mấy nhóm gửi mã + deal nhắc mã khung giờ liên tục mọi người tham gia tránh bỏ lở nhưng thứ hay ho ở nhóm này nhé</p>
+<p>👉 Nhóm tin nhanh mã (chỉ nhận tin từ ad, no chat)</p>
+<p>TGG 3 ➡ <a href="https://zalo.me/g/fqehfa279">https://zalo.me/g/fqehfa279</a></p>
+<p>TGG 2 ➡ <a href="https://zalo.me/g/cvpxza602">https://zalo.me/g/cvpxza602</a></p>
+<p>👉 Nhóm chát ➡ <a href="https://zalo.me/g/vjliqt213">https://zalo.me/g/vjliqt213</a></p><p>--------------------------------------------------------</p>
+<p>😍 Tham gia Group cập nhật Voucher, Deal xịn nha mng 💖</p>
+<p>👉 <a href="https://www.facebook.com/584602429633228" target="_blank">https://www.facebook.com/584602429633228</a></p>
+<p>🍀 Tìm mã giảm giá tại: <a href="https://trumgiamgia.tk" target="_blank">https://trumgiamgia.tk</a></p>
+<p>👉 Mã giảm giá SHOPEE: <a href="https://trumgiamgia.tk/tong-hop/shopee/" target="_blank">https://trumgiamgia.tk/tong-hop/shopee/</a></p>
+<p>👉 Mã giảm giá LAZADA: <a href="https://trumgiamgia.tk/tong-hop/lazada/" target="_blank">https://trumgiamgia.tk/tong-hop/lazada/</a></p>
+<p>👉 Mã giảm giá TIKI: <a href="https://trumgiamgia.tk/tong-hop/tiki/" target="_blank">https://trumgiamgia.tk/tong-hop/tiki/</a></p>
 ${idTime}
 CTVID: TOPVOUCHERM001-CB
 `;
 
-document.getElementById('innerFooter').innerHTML = dataFooter.replaceAll(`
-`,"<br/>");
+document.getElementById('innerFooter').innerHTML = dataFooter;

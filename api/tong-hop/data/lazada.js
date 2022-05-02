3e4cb1052cab1a658320920b4f29f25b23c76b75
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-05-02 11:23:28 +0700

    Update lazada.js

--- api/tong-hop/data/lazada.js
+++ api/tong-hop/data/lazada.js
@@ -1,4 +1,22 @@
-var dataVoucher = ``;
+var dataVoucher = `
+
+🔥MÃ NGâN HÀNG:  back vào 12H - load liên tục có thể lên sớm hoặc muộn (Có thêm mã CIMB giảm 30K)
+=> Lưu mã tại: https://shorten.asia/87QfXZJF 
+.
+🔥LAZMALL: mã mới hàng ngày
+• Mã giảm 60K đơn từ 400K
+=> Lưu mã tại: https://shorten.asia/E2q42YGR
+.
+🔥ZALOPAY: có full mã
+=> Lưu mã tại: https://shorten.asia/FptnxyZD
+.
+🔥MÃ THANH TOÁN GIẢM 15K ĐƠN TỪ 60K: https://shorten.asia/YaQJevBA
+ .
+🔥VOUCHER TÍCH LŨY GIẢM TỚI 400K: ngày 5-9.5dùng
+• Thu thập voucher hàng ngày tại: https://shorten.asia/9rE2bqTa
+• Thu thập thêm voucher tích lũy dưới link sản phẩm: https://shorten.asia/KDr2bXfZ
+
+`;
 
 
 var monthVoucher = `

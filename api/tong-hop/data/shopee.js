8e029d2943ea0d9d1ade5b074540ea9f68873224
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-05-02 09:38:24 +0700

    Create shopee.js

---
+++ api/tong-hop/data/shopee.js
@@ -0,0 +1,35 @@
+var dataVoucher = `
+
+🔥 4/5/22 23:59
+
+Hoàn 5% tối đa 100K Xu cho đơn từ 500K (App): https://bit.ly/3kvwq5e
+
+🔥 10/05/22 23:59
+
+ShopeePay:
+
+Giảm 5% tối đa 50K cho đơn từ 200K: https://bit.ly/3s3MKhT
+
+Giảm ngay 10K cho đơn từ 50K: https://bit.ly/37RWGnM
+
+🔥 31/05/22 35:59
+
+[Hóa đơn]- Giảm 3% tối đa 10000Đ cho đơn từ 0Đ: https://bit.ly/3vxHJ34
+
+[Vé máy bay]- Giảm 5% tối đa 100000Đ cho đơn từ 0Đ: https://bit.ly/3F4bg7w
+
+[Vé xem phim]- Giảm 20% tối đa 20000Đ cho đơn từ 0Đ: https://bit.ly/3kscQqz
+
+[Đặt phòng khách sạn]- Giảm 10% tối đa 100000Đ cho đơn từ 0Đ: https://bit.ly/39vA9NW
+
+🔥 SPP:
+
+Giảm 100K cho đơn hàng từ 0Đ: https://bit.ly/3vYP4rx
+
+Giảm 10% tối đa 20000Đ cho đơn từ 30000Đ: https://bit.ly/3vxL9mA
+
+🔥 MumClub:
+
+Giảm 100K cho đơn từ 0đ (App): https://bit.ly/3F2iYiC
+
+`;

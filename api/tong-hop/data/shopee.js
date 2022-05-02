25481d12f8d21905eef8c0300b6c799b55de065a
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-05-02 09:47:54 +0700

    Update shopee.js

--- api/tong-hop/data/shopee.js
+++ api/tong-hop/data/shopee.js
@@ -1,5 +1,108 @@
 var dataVoucher = `
 
+⏰SHOPEE, LAZADA TỪ 9H - 11H NGÀY 2.05
+
+➖➖➖➖SHOPEE➖➖➖➖
+
+🔥MÃ TOÀN SÀN - NHẬP TAY: canh back lúc 9H - áp liên tục vì có thể lên sớm hoặc muộn hơn vài phút
+
+• 9H: SPIDOLLIVE552 giảm 50% tối đa 30K đơn từ 0Đ. List sản phẩm áp mã: https://shorten.asia/P3RuSf7U (có thể sẽ back)
+
+• 9H: 25GIAM10K0PH hoặc 25GIAM10K1PH hoặc 25GIAM10K2PH giảm 15% tối đa 10K đơn từ 50K
+
+• 9H: 145HOAN100K0PH hoặc 145HOAN100K1PH hoặc 145HOAN100K2PH hoàn 5% tối đa 100K đơn từ 500K
+
+• 9H: 145HOAN40K0PH hoặc 145HOAN40K1PH hoặc 145HOAN40K2PH hoàn 5% tối đa 40K đơn từ 500K
+
+.
+
+🚛MIỄN PHÍ VẬN CHUYỂN:
+
+• 9H: Lưu 1 mã Freeship 0Đ giảm 10K tại: https://shorten.asia/TmjSz6hn (back)
+
+• Đổi mã Freeship giảm 80% - giảm tối đa 40K tại: https://shorten.asia/auYBcR79
+
+.
+
+🔥MÃ ÁP SẢN PHẨM CÓ MÁC “Hoàn Xu Xtra”: canh back 9H
+
+• 9H: Mã hoàn 6% tối đa 200K đơn từ 400K
+
+• 9H: Mã hoàn 10% tối đa 50K đơn từ 99K
+
+=> Lưu mã tại: https://shorten.asia/dDGvZyuR
+
+.
+
+🔥Colos Multi: https://shorten.asia/AaTaSCdc
+
+• 9H: COLOSB555 (mã shop) giảm 555K đơn từ 0Đ
+
+• 9H26s: COLOS25C2 (mã shop) giảm 99K đơn từ 400K
+
+• 9H36s: COLOS25D2 (mã shop) giảm 10% tối đa 150K đơn từ 400K
+
+• COLOSSP25 giảm 25K đơn từ 0Đ 
+
+=> Áp chung list sản phẩm tại: https://shorten.asia/6bQXaZ7B
+
+.
+
+🔥Aviano x Zero Sports: https://shorten.asia/rNDfquUQ
+
+• 9H: SPBT02051 (mã shop) giảm 100K đơn từ 199K cho sản phẩm của Aviano Menswear. List sản phẩm áp mã: https://shorten.asia/2R8rtNbY
+
+• 9H: ZERO02051 (mã shop) giảm 100K đơn từ 199K cho sản phẩm của Zero Sports. List sản phẩm áp mã: https://shorten.asia/VSqJ8ZD3
+
+.
+
+🔥360 Boutique: https://shorten.asia/GuqUw9wS
+
+• 9H: Săn deal 99K tại banner
+
+.
+
+🔥Lock&Lock: https://shorten.asia/Nn8ZbXBA
+
+• 9H: Deal  tự giảm 55K - Bình nước thể thao có ống hút Lock&Lock: https://shorten.asia/ha589e34
+
+.
+
+🔥The Bad God: https://shorten.asia/bbXuJah2
+
+• 9H: Săn voucher 111K đơn từ 333K tại banner hoặc chờ cập nhật nếu có
+
+.
+
+🔥TIÊU DÙNG MALL: https://shorten.asia/XhERxpVc
+
+• 9H: Săn voucher 44K  tại banner hoặc chờ cập nhật nếu có
+
+.
+
+🔥SHOPEEFOOD: https://shorten.asia/JBQ3QVkh
+
+• 11H: Săn mã giảm 50% tối đa 30K đơn từ 0Đ Khi đặt ShopeeFood trên app Shopee - chờ cập nhật
+
+.
+
+🔥MÃ ÁP MỘT SỐ SẢN PHẨM: còn lượt
+
+• AFF5SPP30K giảm 30K đơn từ 150K. List sản phẩm áp mã: https://shorten.asia/bGt6nsYY
+
+• AFF5CPHCM hoàn 20K đơn từ 50K (HCM). List sản phẩm áp mã: https://shorten.asia/x8FGW1H6
+
+• AFFPRE5MA20K giảm 20K đơn từ 99K. List sản phẩm áp mã: https://shorten.asia/pNEncqKx
+
+🔥SIÊU SALE VOUCHER - MÃ TOÀN SÀN CHO KHU VỰC HCM: lọc khu vực + lọc tk mua ít mua nhiều luôn nhé
+• Lưu dùng luôn mã hoàn 20K đơn từ 50K 
+
+=> Lưu mã tại: https://shorten.asia/rWwG8hA4
+
+🔥CHỜ ĐÓN SIÊU SALE 5.5: 
+
+=> Lưu sớm mã tại: https://shorten.asia/kvWyZNEk
+
 
 🔥 4/5/22 23:59
 

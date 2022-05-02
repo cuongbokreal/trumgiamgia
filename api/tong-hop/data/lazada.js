81ae50a0c53da765b68a804e614f69e4ac21a7c6
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-05-02 09:55:40 +0700

    Create lazada.js

---
+++ api/tong-hop/data/lazada.js
@@ -0,0 +1,12 @@
+var dataVoucher = ``;
+
+
+var monthVoucher = `
+🔥 31/05/22 23:59
+[ZaloPay New Users] Giảm 15K cho đơn từ 30K: https://bit.ly/3KrUccU
+[ZaloPay Home Living] Giảm 15K cho đơn từ 99K: https://bit.ly/3kwKawq
+[ZaloPay Home Living] Giảm 15K cho đơn từ 99K: https://bit.ly/3MCBCAa
+[Momo New Users] Giảm 20K cho đơn từ 50K: https://bit.ly/3vYPvBV
+`;
+
+dataVoucher += monthVoucher;

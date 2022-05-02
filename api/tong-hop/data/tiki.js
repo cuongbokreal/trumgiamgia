e4616fe40dfb0084f7d65e54a92ba3fe0a92b33c
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-05-02 20:53:32 +0700

    Update tiki.js

--- api/tong-hop/data/tiki.js
+++ api/tong-hop/data/tiki.js
@@ -2,6 +2,131 @@ var dataVoucher = ``;
 
 
 var monthVoucher = `
+🔥 5/5/22 23:59
+
+📌 Tiki Ngon:
+Giảm 20K cho đơn từ 199K: https://bit.ly/3KzNRMG
+Giảm 30K cho đơn từ 299K: https://bit.ly/3kznThu
+
+
+🔥 31/05/22 23:59
+
+📌 TIKI New User:
+Giảm 20K cho đơn từ 99K: https://bit.ly/3s79gpR
+[Tiki Ngon] Giảm 20K cho đơn hàng từ 99K: https://bit.ly/3upmoc0
+[Tiki Ngon] Giảm 50K cho đơn hàng từ 199K: https://bit.ly/3OOHyYN
+[Tiki Ngon] Giảm 50K cho đơn từ 199K: https://bit.ly/3N1TTHB
+
+📌 Tiki Ngon:
+Giảm 10K phí vận chuyển cho đơn từ 149K: https://bit.ly/3yohi1R
+Giảm 17K phí vận chuyển cho đơn từ 149K: https://bit.ly/3w4zxqi
+Giảm 25K phí vận chuyển cho đơn từ 249K: https://bit.ly/375wLs8
+Giảm 15K cho đơn từ 199K: https://bit.ly/3vzAo37
+Giảm 25K cho đơn từ 299K: https://bit.ly/38Fp5gI
+Giảm 30K cho đơn từ 199K: https://bit.ly/3KAOPbe
+Giảm 40K cho đơn từ 249K: https://bit.ly/3vwpqvc
+Giảm 40K cho đơn từ 399K: https://bit.ly/3y8mXso
+Giảm 70K cho đơn từ 399K: https://bit.ly/3w0ntWK
+
+📌 Ví Moca:
+Giảm 20K cho đơn từ 199K: https://bit.ly/3LFPBp0
+Giảm 70K cho đơn từ 799K: https://bit.ly/3FbfbQi
+
+📌 Ví Vnpay:
+- VNPAY18K giảm 18K đơn 150K: https://shorten.asia/Bpm8P9Uz
+
+🔥 Các Banner:
+📌 Banner chính Sale hè: https://shorten.asia/NhyG3FBb
+📌 Lịch sự kiện: https://shorten.asia/hnsxcKrk
+📌 Beauty Club: https://shorten.asia/X5m1zfsU
+📌 MumClub: https://shorten.asia/ZFmSVrRq
+📌 Tiki mách mẹ: https://shorten.asia/1HDMTKGn
+📌 Mọt sách Tiki: https://shorten.asia/ZKZv5hSc
+📌 Bách hóa Online: https://shorten.asia/a57WbuC5
+📌 Mã giảm giá FMGG: https://shorten.asia/dN8MDq9s
+📌 Săn sale điện tử, miễn phí giao lắp: https://shorten.asia/dVPurkz1
+📌 Tech Area: https://shorten.asia/emVJ8BUc
+📌 Flash sale điện gia dụng: https://shorten.asia/tK5CrX1u
+📌 Top điện máy: https://shorten.asia/U3nnq2dN
+📌 Siêu deal nhà cửa: https://shorten.asia/47yJSeMr
+📌 Ưu đãi Visa: https://shorten.asia/ajFy8WtK
+📌 Ưu đãi VNPay: https://shorten.asia/BAj4MUdK
+
+Gian hàng Brand:
+🔥 Từ Benguna (Đồ điện tử + game): https://shorten.asia/9X3dTC3J
+- Giảm  35K cho đơn từ 399K: https://bit.ly/3MBFMZ9
+- Giảm  80K cho đơn từ 799K: https://bit.ly/36XB7S2
+.
+🔥 Từ Lykry (Đồng hồ): https://shorten.asia/B9qNVgUa
+- Giảm  5% tối đa 25K cho đơn từ 399K: https://bit.ly/3MviJ2a
+- Giảm  7% tối đa 70K cho đơn từ 699K: https://bit.ly/3Lv0Bp0
+.
+🔥 Từ iBasic Official Store (Thời trang): https://shorten.asia/CdS6euvr
+- Giảm 5% cho đơn từ 199K: https://bit.ly/3OLqP8H
+- Giảm 8% cho đơn từ 399K: https://bit.ly/3OIoCLl
+- Giảm 50K cho đơn từ 599K: https://bit.ly/3s1U0up
+- Giảm 100K cho đơn từ 1tr: https://bit.ly/3vVYLqN
+- Giảm 200K cho đơn từ 2tr: https://bit.ly/3LwrHMj
+.
+🔥 Từ VuaCua (Thực phẩm): https://shorten.asia/AjEa38Wa
+- Giảm 20K cho đơn từ 199K: https://bit.ly/3MJFH5T
+- Giảm 30K cho đơn từ 299K: https://bit.ly/3rZHcow
+- Giảm 50K cho đơn từ 399K: https://bit.ly/3LwszR5
+.
+🔥 Từ Farm49 (Thực phẩm): https://shorten.asia/eVhUThW4
+- Giảm 10K cho đơn từ 149K: https://bit.ly/3F2CyeQ
+- Giảm 20K cho đơn từ 199K: https://bit.ly/3kpvaAI
+- Giảm 20K cho đơn từ 230K: https://bit.ly/3ONnR3J
+- Giảm 30K cho đơn từ 299K: https://bit.ly/3MDt3oJ
+- Giảm 30K cho đơn từ 339K: https://bit.ly/3xZw7rl
+- Giảm 50K cho đơn từ 399K: https://bit.ly/3KsV3Kp
+.
+🔥 Từ Máy lọc nước Karofi (Điện tử): https://shorten.asia/dapVhJcF
+- Giảm 200K cho đơn từ 5tr: https://bit.ly/3s3PLhZ
+- Giảm 300K cho đơn từ 8tr: https://bit.ly/3MDrpn3
+- Giảm 500K cho đơn từ 10.5tr: https://bit.ly/3kvjg85
+- Giảm 800K cho đơn từ 14tr: https://bit.ly/3knsEuq
+.
+🔥 Từ Moriitalia (Thực phẩm + đồ gia dụng): https://shorten.asia/8sm1sbhJ
+- Giảm 5% cho đơn từ 199K: https://bit.ly/3y2Esur
+- Giảm 50K cho đơn từ 1tr: https://bit.ly/3knsNht
+.
+🔥 Từ Mishio Kachi (Dụng cụ nhà cửa + thể thao): https://shorten.asia/aYpxxwS3
+- Giảm 10% cho đơn từ 300K: https://bit.ly/3kmNjiv
+- Giảm 50K cho đơn từ 500K: https://bit.ly/36Z0Rh5
+- Giảm 100K cho đơn từ 1tr: https://bit.ly/3rWtrGT
+- Giảm 200K cho đơn từ 2tr: https://bit.ly/3vKHaBW
+- Giảm 300K cho đơn từ 3tr: https://bit.ly/36YZsHj
+- Giảm 500K cho đơn từ 5tr: https://bit.ly/39jVYjj
+🔥 ANESSA - Chống nắng dưỡng da: https://shorten.asia/ErAnnk99
+.
+🔥 Từ LOCK&LOCK:
+-  LOCKHOT150K05 Giảm 150k đơn 800k.
+-  LOCKHOT500K05 Giảm 500k đơn 2tr3.
+=> Lưu mã tại: https://shorten.asia/snmkVefv
+.
+🔥 Từ Pepsi: https://shorten.asia/2eFSuvaj
+Giảm 15K cho đơn từ 150K: https://bit.ly/377d9nv
+.
+🔥 Từ NIKITA:
+- NIKA502 Giảm 50K cho đơn từ 700K.
+- NIKA500 Giảm 500K cho đơn từ 6tr.
+=> Lưu mã tại: https://shorten.asia/3BJKJEDV
+.
+🔥 Từ BG Việt Nam:
+- BG419BG Giảm 500K cho đơn từ 5tr
+=> Lưu mã tại: https://shorten.asia/Jn1vCKft 
+.
+🔥 Từ Vua nệm:
+-  VNEM1TR3 Giảm 1tr3 đơn từ 15tr.
+- VNEM600 Giảm 600k đơn từ 7tr5.
+=> Lưu mã tại: https://shorten.asia/EAj5zGrB
+.
+🔥 Từ Danido: 
+- 1H1J1 Giảm 50K cho đơn từ 1tr.
+- 1J1J1J1J Giảm 100K cho đơn từ 1.5tr.
+=> Lưu mã tại: https://shorten.asia/4japxCHp
+
 `;
 
 dataVoucher += monthVoucher;

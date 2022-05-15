6815d6d0ab92bd59da37263eb75bf7a78b079405
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-05-15 08:48:39 +0700

    Update lazada.js

--- api/tong-hop/data/lazada.js
+++ api/tong-hop/data/lazada.js
@@ -1,38 +1,32 @@
 var dataVoucher = `
 
-Mã nhập tay 30K/99K, 50K/299K nếu có mình up trên nhóm zalo https://zalo.me/g/fqehfa279 và tele(lên bất ngờ)
-🔥MÃ TOÀN SÀN: back vào 9H, 12H, 16H, 20H
-• 0H: Mã toàn sàn giảm 30K/500K, 90K/1tr5(dự kiến, đúng giờ mới hiện mã)
-• 0H: Các mã freeship + freeship giảm 25K/99K(mã này hết nhanh)
-=> Lưu mã tại: https://shorten.asia/gCAFAXCB
-.
-🔥THỜI TRANG NỮ:
-• 20H: Lưu mã giảm 200K đơn từ 500K từ các thương hiệu thời trang
-=> Lưu mã tại: https://shorten.asia/R7vS5VKv
-.
-🔥VOUCHER TÍCH LŨY GIẢM TỚI 400K: từ 15-19.5 mới dùng được
-• Thu thập voucher tích luỹ hàng ngày tại: https://shorten.asia/gCAFAXCB
-• Thu thập thêm voucher tích luỹ + làm nhiệm vụ tại: https://shorten.asia/wFbXd35N
-• Thu thập thêm voucher tích lũy dưới link sản phẩm: https://shorten.asia/zPUv1Zyx
+Mã nhập tay 30K/99K, 50K/299K nếu có mình up trên nhóm zalo và tele(lên bất ngờ)
+🔥MÃ TOÀN SÀN: canh back
+• 9H: Mã toàn sàn giảm 30K/500K, 500K/0Đ
+=> Lưu mã tại: https://shorten.asia/g7EyC8rS
 .
 🔥MOMO: canh back 9H hàng ngày - có thể lên sớm hoặc muộn vài phút
-=> Lưu mã tại: https://shorten.asia/N9c3HGcn
+=> Lưu mã tại: https://shorten.asia/yrgr3Sye
 .
 🔥ZALOPAY: canh back 9H hàng ngày - có thể lên sớm hoặc muộn vài phút
-=> Lưu mã tại: https://shorten.asia/53gK2sV4
+=> Lưu mã tại: https://shorten.asia/REm47Kuf
 .
 🔥MÃ NGâN HÀNG: mở lưu vào 9H, back vào 12H
-=> Lưu mã tại: https://shorten.asia/dGSUcRUf
+=> Lưu mã tại: https://shorten.asia/bUA5FU7K
 .
 🔥LAZMALL: mã mới hàng ngày
 • Mã giảm 60K đơn từ 400K
-=> Lưu mã tại: https://shorten.asia/A2ZmXp75  (chọn “SĂN VOUCHER TẠI ĐâY”)
+=> Lưu mã tại: https://shorten.asia/tnaamDxJ  (chọn “SĂN VOUCHER TẠI ĐâY”)
 .
-🔥MÃ ĐỜI SỐNG: 0H, 9H, 12H, 16H có thể back mã
-=> Lưu mã tại: https://shorten.asia/ws9B26U1
+🔥MÃ THANH TOÁN GIẢM 15K ĐƠN TỪ 60K: https://shorten.asia/eh1VZCBE
 .
-🔥FLASH SALE: https://shorten.asia/qsKfk1wh
-• 0H, 12H: Mua deal sốc(sẽ hiện hết giá trước 5-15phút)
+🔥MÃ QUỐC TẾ: canh back 9H
+=> Lưu mã tại: https://shorten.asia/q1Emgu3V
+🔥VOUCHER TÍCH LŨY GIẢM TỚI 400K: đã dùng được
+• Thu thập voucher tích luỹ hàng ngày tại: https://shorten.asia/g7EyC8rS
+• Thu thập thêm voucher tích luỹ + làm nhiệm vụ tại: https://shorten.asia/QRwrjjuQ
+• Thu thập thêm voucher tích lũy dưới link sản phẩm: https://shorten.asia/ar1cRWKU
+
 
 `;
 

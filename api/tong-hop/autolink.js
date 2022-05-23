5f04a43185ed17844950c643c4a8c69c8f204c91
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-05-23 12:27:23 +0700

    Update autolink.js

--- api/tong-hop/autolink.js
+++ api/tong-hop/autolink.js
@@ -1,3 +1,3 @@
 $("#innerMain").each(function(){
-   $(this).html( $(this).html().replace(/((http|https|ftp):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g, '<a href="$1">$1</a> ') );
+   $(this).html( $(this).html().replace(/((http|https|ftp):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g, '<a target="_blank" href="$1">$1</a> ') );
 });

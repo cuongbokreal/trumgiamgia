cd83c2461163c9e953fb6fd89e456edec7adaf82
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-05-24 18:28:43 +0700

    Create data.js

---
+++ api/tong-hop/thbao/data.js
@@ -0,0 +1,3 @@
+// <p id="thbao" class="note"></p>
+
+document.getElementById('thbao').innerHTML = `Tham gia group Zalo nhận tin tổng hợp các chương trình sale sớm nhất: https://trumgiamgia.tk/tong-hop/group/zalo/`;

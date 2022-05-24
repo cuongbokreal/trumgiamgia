8c87cff0f4ab07495d5fa05af231a1b750b07b5e
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-05-24 18:30:17 +0700

    Update data.js

--- api/tong-hop/thbao/data.js
+++ api/tong-hop/thbao/data.js
@@ -1,3 +1,3 @@
 // <p id="thbao" class="note"></p>
 
-document.getElementById('thbao').innerHTML = `Tham gia group Zalo nhận tin tổng hợp các chương trình sale sớm nhất: https://trumgiamgia.tk/tong-hop/group/zalo/`;
+document.getElementById('thbao').innerHTML = `Tham gia group Zalo nhận tin tổng hợp các chương trình sale sớm nhất: <a href="https://trumgiamgia.tk/tong-hop/group/zalo/" target="_blank">https://trumgiamgia.tk/tong-hop/group/zalo/</a>`;

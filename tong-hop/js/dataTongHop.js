b3c43e8ed630e1b63aca1c2a0759dd21a02f3f07
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-06-16 11:56:09 +0700

    Update dataTongHop.js

--- tong-hop/js/dataTongHop.js
+++ tong-hop/js/dataTongHop.js
@@ -1,41 +1,14 @@
- MÃ HOT 21H NGÀY 15.6:
-🔥KOL - MÃ TOÀN SÀN - LƯU BANNER: đúng giờ load link mới hiện mã
-• 21H: Mã giảm 30K đơn từ 99K(lọc tài khoản)
-• 21H: Mã giảm 10K đơn từ 10K(không lọc tài khoản)
-=> Lưu mã tại: https://shorten.asia/hgp7SSbr
+➖➖➖➖LAZADA➖➖➖➖
+🔥MÃ TOÀN SÀN: canh back - có thể lên sớm hoặc muộn vài phút
+• 12H, 16H: Mã toàn sàn giảm 30K/500K, 500K/0Đ(back)
+=> Lưu mã tại: https://shorten.asia/yUy7aBHv
 .
-🔥MÃ TOÀN SÀN - NHẬP TAY - KHÔNG LỌC TÀI KHOẢN:
-• 21H: 156HUNT156K21H giảm 15% tối đa 156K đơn từ 156K
-• 21H: 156GIAM9K giảm 9K đơn từ 0Đ(back)
+🔥12H: MÃ NGâN HÀNG: canh back - có thể lên sớm hoặc muộn vài phút
+=> Lưu mã tại: https://shorten.asia/2A1HGdQq
 .
-🔥Lorealparis Maybelline: https://shorten.asia/mu3qjF5Z
-• 21H: SANGDA21H giảm 100K đơn từ 350K. List sản phẩm áp mã: https://shorten.asia/tCBsbpc2
+🔥ZALOPAY: còn lượt
+=> Lưu mã tại: https://shorten.asia/bnkaU62g
 .
-🔥MÃ TOÀN SÀN - NHẬP TAY - LỌC TÀI KHOẢN:
-• 21H: 156GIAM70K21H0 hoặc 156GIAM70K21H1 hoặc 156GIAM70K021H2 giảm 7% tối đa 70K đơn từ 500K
-• 21H: 156HOAN15K21H0 hoặc 156HOAN15K21H1 hoặc 156HOAN15K021H2 hoàn 15% tối đa 15K đơn từ 99K
-• 21H: 156MALL50K21H0 hoặc 156MALL50K21H1 hoặc 156MALL50K21H2 giảm 7% tối đa 50K đơn từ 250K(Mall)
-• 21H: Mã kol 30K nhập tay chờ cập nhật trên zalo, tele NẾU CÓ!
-.
-🔥MÃ ÁP SẢN PHẨM CÓ MÁC “Hoàn Xu Xtra”: có thể sẽ back
-• 21H: Mã hoàn 10% tối đa 300K đơn từ 1triệu(back)
-• 21H: Mã hoàn 7% tối đa 200K đơn từ 400K(back)
-• 21H: Mã giảm 5% tối đa 100K đơn từ 500K(back)
-=> Lưu mã tại: https://shorten.asia/Hus4aEJB
-.
-🔥MÃ ÁP MỘT SỐ SẢN PHẨM: canh back 21H
-• AFF156BNBG giảm 30K đơn từ 150K. List sản phẩm áp mã: https://shorten.asia/bQRkNbxJ
-• AFF156CPALL hoàn 20K đơn từ 50K. List sản phẩm áp mã: https://shorten.asia/WuZcYNMr
-.
-🔥TIÊU DÙNG: https://shorten.asia/DzvzBRV8
-• 21H: 200KXU5 (mã shop) hoàn 50% tối đa 200K đơn từ 0Đ. List sản phẩm áp mã: https://shorten.asia/VcfEHvym
-• 21H: 200KXU6 (mã shop) hoàn 50% tối đa 200K đơn từ 0Đ. List sản phẩm áp mã: https://shorten.asia/uqpN79ER
-• 21H: 200KXU7 (mã shop) hoàn 50% tối đa 200K đơn từ 0Đ. List sản phẩm áp mã: https://shorten.asia/7fJJq5RP
-• 21H: 200KXU8 (mã shop) hoàn 50% tối đa 200K đơn từ 0Đ. List sản phẩm áp mã: https://shorten.asia/uNWzxMSt
-.
-🔥NẠP THẺ - DỊCH VỤ: https://shorten.asia/RA2xA87S
-• 21H: Săn ưu đãi nạp điện thoại 50K chỉ 6K
-• 21H: Lưu mã mã giảm 50% tối đa 66K đơn từ 0Đ cho thanh toán hóa đơn(back)
-.
-🔥THỜI TRANG: https://shorten.asia/UkeG84Yp
-• 21H: Săn mã shop giảm 220K tại banner: https://shorten.asia/UkeG84Yp
+🔥VOUCHER TÍCH LŨY GIẢM TỚI 400K: dùng từ ngày 15.6-19.6
+• Lưu 30K tích luỹ tại: https://shorten.asia/gvHg8xNg
+• Lưu thêm 30K tích luỹ dưới sản phẩm tại: https://shorten.asia/13wXM9nd

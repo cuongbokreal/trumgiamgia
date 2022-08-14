b0e07a861cd470df235c8c3af17d384c6b656f4f
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-08-14 16:48:56 +0700

    Update get-voucher-details-mobile.js

--- api/tiki/get-voucher-details-mobile.js
+++ api/tiki/get-voucher-details-mobile.js
@@ -1,24 +1,25 @@
-//var listCoupon = document.querySelector('#__next > div.page-builder-desktop > main > div > div:nth-child(24) > div > div > div > div').childNodes;
-var listCoupon = document.querySelector('#__next > div.page-builder-desktop > main > div > div:nth-child(30) > div > div > div > div').childNodes;
-
+//var listCoupon = document.querySelector('#mobile-container > div > div:nth-child(23) > div > div').childNodes;
+var listCoupon = document.querySelector('#mobile-container > div > div:nth-child(29) > div > div).childNodes;
 
     
 Object.keys(listCoupon).forEach(key => {
 	var coupon = listCoupon[key].childNodes[0].childNodes[0].childNodes[1];
-	var coupon_left = coupon.childNodes[1];
+    console.log(coupon)
+    
+    var coupon_left = coupon.childNodes[1];
     var coupon_right = coupon.childNodes[2];
+    var counpon_image = coupon_left.childNodes[0].childNodes[0];
+    console.log(counpon_image)
     
-    //var counpon_image = coupon.childNodes[1].childNodes[0].childNodes[0].childNodes[0].src; //img
-    //var counpon_image = coupon.childNodes[1].childNodes[0].childNodes[0].childNodes[0].childNodes[0].src;//a
-    
-    var counpon_image = coupon.childNodes[1].childNodes[0].childNodes[0].childNodes[0];
-    if(counpon_image.tagName === 'DIV'){
-    	counpon_image = coupon.childNodes[1].childNodes[0].childNodes[0].childNodes[0].childNodes[0].src;//a
+})
+
+
+
+if(counpon_image.tagName === 'DIV'){
+    	counpon_image = counpon_image.childNodes[0].childNodes[0].childNodes[0].src;//a
     }else{
-    	counpon_image = coupon.childNodes[1].childNodes[0].childNodes[0].childNodes[0].src; //img
+    	counpon_image = counpon_image.childNodes[0].childNodes[0].src; //img
     }
     
-    var coupon_details_button = coupon_right.childNodes[0];
-    coupon_details_button.click();
-    console.log(document.getElementById('popup-root').innerText)
-})
+
+

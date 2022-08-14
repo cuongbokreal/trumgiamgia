161d935b5e65279fdf9c7d3212142ab757e18e0c
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-08-14 17:00:57 +0700

    Update get-voucher-details-mobile.js

--- api/tiki/get-voucher-details-mobile.js
+++ api/tiki/get-voucher-details-mobile.js
@@ -1,19 +1,30 @@
-//var listCoupon = document.querySelector('#mobile-container > div > div:nth-child(23) > div > div').childNodes;
-var listCoupon = document.querySelector('#mobile-container > div > div:nth-child(29) > div > div').childNodes;
+var listCoupon = document.querySelector('#mobile-container > div > div:nth-child(23) > div > div').childNodes;
+//var listCoupon = document.querySelector('#mobile-container > div > div:nth-child(29) > div > div').childNodes;
 
-    
+var couponModal = document.getElementById('coupon-modal-container').childNodes[0].childNodes[0];
+
 Object.keys(listCoupon).forEach(key => {
 	var coupon = listCoupon[key].childNodes[0].childNodes[0].childNodes[1];
     
     var coupon_left = coupon.childNodes[1];
     var coupon_right = coupon.childNodes[2];
+    
     var counpon_image = coupon_left.childNodes[0].childNodes[0].childNodes[0];
-
     if(counpon_image.tagName === 'IMG'){
     	counpon_image = counpon_image.src; //img
     }else{
     	counpon_image = counpon_image.childNodes[0].src; //a
     }
-    console.log(counpon_image)
+    
+    var coupon_code = couponModal.childNodes[2].childNodes[1].innerText;
+    var coupon_expired = couponModal.childNodes[3].childNodes[1].innerText;
+    var coupon_des =  couponModal.childNodes[4].childNodes[1].innerText;
+    
+    var coupon_details_button = coupon_right.childNodes[0];
+    coupon_details_button.click();
 
 })
+
+
+
+
